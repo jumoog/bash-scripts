c09357e178d622fb0d9588832a5130f27733283e
Kilian von Pflugk <jumoog@users.noreply.github.com>
2015-10-24 17:37:23 +0200

    blame me

--- translation.sh
+++ translation.sh
@@ -2,10 +2,13 @@
 # copy translation like a boss
 # 2015 jumoog
 # script.sh $(cat list.txt)
-for i in res/values-*/cm_strings.xml; do
+for i in packages/SystemUI/res/values-*/cm_strings.xml; do
+        c3po=$(dirname ${i})
+        mkdir -p ${c3po}
+        echo '<?xml version="1.0" encoding="utf-8"?>' >> ../android_frameworks_base/${c3po}/cm_string.xml
+        echo '<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">' >> ../android_frameworks_base/${c3po}/cm_string.xml
         for y in "${@}"; do
-                c3po=$(dirname ${i})
-                mkdir -p ${c3po}
-                sed -n "/${y}/p" ${i} >> ../android_packages_apps_Settings/${c3po}/cm_string.xml
+                sed -n "/${y}/p" ${i} >> ../android_frameworks_base/${c3po}/cm_string.xml
         done;
+        echo '</resources>' >> ../android_frameworks_base/${c3po}/cm_string.xml
 done;
