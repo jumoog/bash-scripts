ffbdcb170e20b2a442d66ae44c78f0712f68b5f6
Kilian von Pflugk <jumoog@users.noreply.github.com>
2015-10-26 23:04:58 +0100

    Update translation.sh

--- translation.sh
+++ translation.sh
@@ -2,13 +2,15 @@
 # copy translation like a boss
 # 2015 jumoog
 # script.sh $(cat list.txt)
-for i in packages/SystemUI/res/values-*/cm_strings.xml; do
+for i in core/res/res/values-*/cm_strings.xml; do
         c3po=$(dirname ${i})
-        mkdir -p ${c3po}
+        rm -rf ../android_frameworks_base/${c3po}/cm_string.xml
         echo '<?xml version="1.0" encoding="utf-8"?>' >> ../android_frameworks_base/${c3po}/cm_string.xml
         echo '<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">' >> ../android_frameworks_base/${c3po}/cm_string.xml
         for y in "${@}"; do
-                sed -n "/${y}/p" ${i} >> ../android_frameworks_base/${c3po}/cm_string.xml
+                sed -n "/${y}/, /<\/string>/p" ${i} >> ../android_frameworks_base/${c3po}/cm_string.xml
         done;
         echo '</resources>' >> ../android_frameworks_base/${c3po}/cm_string.xml
+        awk '!a[$0]++' ../android_frameworks_base/${c3po}/cm_string.xml > ../android_frameworks_base/${c3po}/cm_string.xm
+        mv ../android_frameworks_base/${c3po}/cm_string.xm ../android_frameworks_base/${c3po}/cm_string.xml
 done;
