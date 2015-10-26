4e3886d4fa9d110a9a8fdc5497c1ac191a0ad13a
Kilian von Pflugk <jumoog@users.noreply.github.com>
2015-10-26 23:08:39 +0100

    Update translation.sh

--- translation.sh
+++ translation.sh
@@ -4,13 +4,14 @@
 # script.sh $(cat list.txt)
 for i in core/res/res/values-*/cm_strings.xml; do
         c3po=$(dirname ${i})
-        rm -rf ../android_frameworks_base/${c3po}/cm_string.xml
-        echo '<?xml version="1.0" encoding="utf-8"?>' >> ../android_frameworks_base/${c3po}/cm_string.xml
-        echo '<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">' >> ../android_frameworks_base/${c3po}/cm_string.xml
+        target=android_frameworks_base
+        rm -rf ../${target}/${c3po}/cm_string.xml
+        echo '<?xml version="1.0" encoding="utf-8"?>' >> ../${target}/${c3po}/cm_string.xml
+        echo '<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">' >> ../${target}/${c3po}/cm_string.xml
         for y in "${@}"; do
-                sed -n "/${y}/, /<\/string>/p" ${i} >> ../android_frameworks_base/${c3po}/cm_string.xml
+                sed -n "/${y}/, /<\/string>/p" ${i} >> ../v/${c3po}/cm_string.xml
         done;
-        echo '</resources>' >> ../android_frameworks_base/${c3po}/cm_string.xml
-        awk '!a[$0]++' ../android_frameworks_base/${c3po}/cm_string.xml > ../android_frameworks_base/${c3po}/cm_string.xm
-        mv ../android_frameworks_base/${c3po}/cm_string.xm ../android_frameworks_base/${c3po}/cm_string.xml
+        echo '</resources>' >> ../${target}/${c3po}/cm_string.xml
+        awk '!a[$0]++' ../${target}/${c3po}/cm_string.xml > ../v/${c3po}/cm_string.xm
+        mv ../${target}/${c3po}/cm_string.xm ../${target}/${c3po}/cm_string.xml
 done;
